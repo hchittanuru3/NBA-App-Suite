1b696c45baa3240c78955c71bc2fe5a7573cde02
hchittanuru3 <hemanthc98@outlook.com>
2018-07-10 15:46:31 -0700

    First Commit

---
+++ back-end/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const mongo = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 8000;
+
+
+app.listen(port, function() {
+    console.log("App listening on port 8000.");
+});
